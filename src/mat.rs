a8362b54b39417dea8ee4bbd0ac6ab58d2f7d0b7
eholmes21601 <eltonholmes1980@gmail.com>
2022-09-12 03:20:47 -0400

    added comments

--- src/mat.rs
+++ src/mat.rs
@@ -1,4 +1,4 @@
-use ndarray::{arr1, arr2};
+use ndarray::{arr1, arr2, Array1};
 
 pub fn run()
 {
@@ -22,7 +22,13 @@ println!("B:\n {}", b);
 println!("C:\n {}", c);
 println!("The scalar: {}\n", scalar);
 println!("The vector v:\n {}", vector);
+// Matrix Addition
 println!("\n\nThe matrix sum, A + B = \n {}", sum);
+// Matrix Multiplication
 println!("The matrix multiplication, A * C =\n {}", a.dot(&c));
-println!("The scalar multiple, 4v is: {}", scalar * vector);
+// Scalar Multiplication
+let new_vector: Array1<_> = scalar * vector;
+println!("The scalar multiple, 4v is: {}\n", new_vector);
+// Multiple Matrix Operations
+println!("The A * 4v is: {}\n", a.dot(&new_vector));
 }
